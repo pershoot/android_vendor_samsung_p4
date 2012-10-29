26f08de56a9eb7de409ca34d5a5256872774fa7e
Prashant Somashekar <prashsomash@gmail.com>
2012-10-29 11:38:40 -0400

    p4: remove sec_keyboard

--- p4-vendor-blobs.mk
+++ p4-vendor-blobs.mk
@@ -20,7 +20,6 @@ PRODUCT_COPY_FILES := \
     vendor/samsung/p4/proprietary/bin/gpsd:system/bin/gpsd \
     vendor/samsung/p4/proprietary/bin/gps.cer:system/bin/gps.cer \
     vendor/samsung/p4/proprietary/bin/macloader:system/bin/macloader \
-    vendor/samsung/p4/proprietary/bin/sec_keyboard:system/bin/sec_keyboard \
     vendor/samsung/p4/proprietary/bin/tegrastats:system/bin/tegrastats \
     vendor/samsung/p4/proprietary/etc/audio/Rx_ControlParams_BLUETOOTH_HEADSET.txt:system/etc/audio/Rx_ControlParams_BLUETOOTH_HEADSET.txt \
     vendor/samsung/p4/proprietary/etc/audio/Tx_ControlParams_BLUETOOTH_HEADSET.txt:system/etc/audio/Tx_ControlParams_BLUETOOTH_HEADSET.txt \
